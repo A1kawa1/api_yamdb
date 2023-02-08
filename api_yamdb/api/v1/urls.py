ae947cf24e9dad864c59f53fa7ce64aa6b943519
A1kawa1 <113998115+A1kawa1@users.noreply.github.com>
2023-02-08 04:41:12 +0300

    1 review

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,5 +1,5 @@
 from api.v1.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                       ReviewViewSet, TitleViewSet, UserViewSet)
+                          ReviewViewSet, TitleViewSet, UserViewSet)
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
